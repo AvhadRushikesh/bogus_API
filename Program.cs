01e543df337d1760290bfc7825ab5dd6278b37b0
smbios-ra <rushi@smbios.co.in>
2023-12-29 15:01:45 +0530

    Configure Serilog and Seq

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//  CORS Configuration
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", 
@@ -14,6 +17,9 @@ builder.Services.AddCors(options =>
         .AllowAnyOrigin()
         .AllowAnyMethod());
 });
+
+//  Configure Serilog and Seq
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
